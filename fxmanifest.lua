777dbda8efa67a11eb1e55c6662f789e4f862cdf
Artmines <arturowells@gmail.com>
2026-04-27 21:08:57 -0230

    feat(init):initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version("cerulean")
+games({ "gta5" })
+lua54("yes")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+
+client_scripts({
+	"config/*.lua",
+	"client/utils.lua",
+	"client/main.lua",
+	"client/menu.lua",
+	"client/bindings.lua",
+	"client/emotes.lua",
+	"client/ptfxsync.lua",
+	"client/pedfeatures.lua",
+	"client/sharedemotes.lua",
+	"client/pointing.lua",
+	"client/items.lua",
+	"client/chairs.lua",
+	"client/selfie.lua",
+})
+
+server_scripts({
+	"config/*.lua",
+	"server/*.lua",
+})
+
+shared_scripts({
+	"shared/**/*.lua",
+})
